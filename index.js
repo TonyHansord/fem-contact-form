b46b7b70a51811777184b95deb5007e886dba0b5
Anthony Hansord <tonyhansorddev@gmail.com>
2024-06-09 18:29:11 +1000

    fixed issue with modal showing on load

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ const validateForm = () => {
 
 const showModal = () => {
   const modal = document.querySelector('.modal')
+  modal.style.display = 'block'
   modal.classList.remove('hidden')
 
   setTimeout(() => {
